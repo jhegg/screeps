a6ec72c97e36d630f92a5eba594b341afd4e5d64
jhegg <josh.hegg@gmail.com>
2016-11-30 21:39:41 -0800

    Remove debug message

--- role.utility.js
+++ role.utility.js
@@ -1,6 +1,5 @@
 var roleUtility = {
   containersWithEnergy: function(energyStorageStructures) {
-    //console.log(energyStorageStructures);
     return _.filter(energyStorageStructures, (structure) =>
       structure.structureType === STRUCTURE_CONTAINER &&
       structure.store[RESOURCE_ENERGY] > 50
