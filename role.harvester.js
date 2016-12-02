2e6cd37a955ffb19f7f7a77110f0fb1faa537050
jhegg <josh.hegg@gmail.com>
2016-12-01 21:08:41 -0800

    Refactor harvester role

--- role.harvester.js
+++ role.harvester.js
@@ -1,38 +1,62 @@
 var roleHarvester = {
-
   run: function(creep, energyStorageStructures) {
-    if (creep.carry.energy < creep.carryCapacity) {
-      const source = Game.getObjectById(creep.memory.sourceId);
-      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(source);
-      }
+    if (creepNeedsEnergyToCarry(creep)) {
+      harvestEnergyFromSourceMemory(creep);
     } else {
-      if (energyStorageStructures.length > 0) {
-        // prioritize Extension -> Spawn -> Tower -> Container
-        const extensions = _.filter(energyStorageStructures, function(structure) {
-          return structure.structureType === STRUCTURE_EXTENSION &&
-            structure.energy < structure.energyCapacity;
-        });
-        const spawns = _.filter(energyStorageStructures, function(structure) {
-          return structure.structureType === STRUCTURE_SPAWN &&
-            structure.energy < structure.energyCapacity;
-        });
-        const towers = _.filter(energyStorageStructures, function(structure) {
-          return structure.structureType === STRUCTURE_TOWER &&
-            structure.energy < structure.energyCapacity;
-        });
-        const containers = _.filter(energyStorageStructures, function(structure) {
-          return structure.structureType === STRUCTURE_CONTAINER &&
-            _.sum(structure.store) < 2000;
-        });
-        const newSortedStructures = _.flatten([extensions, spawns, towers, containers]);
+      deliverEnergyToStructures(creep, energyStorageStructures);
+    }
+  },
+};
 
-        if (creep.transfer(newSortedStructures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(newSortedStructures[0]);
-        }
-      }
+function creepNeedsEnergyToCarry(creep) {
+  return creep.carry.energy < creep.carryCapacity;
+}
+
+function harvestEnergyFromSourceMemory(creep) {
+  const source = Game.getObjectById(creep.memory.sourceId);
+  if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+    creep.moveTo(source);
+  }
+}
+
+function deliverEnergyToStructures(creep, structures) {
+  if (structures.length > 0) {
+    const sortedStructures = prioritizeStructures(structures);
+    if (creep.transfer(sortedStructures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sortedStructures[0]);
     }
   }
-};
+}
+
+function prioritizeStructures(structures) {
+  // prioritize Extension -> Spawn -> Tower -> Container
+  const extensions = filterStructuresByTypeAndEnergy(structures,
+    STRUCTURE_EXTENSION);
+  const spawns = filterStructuresByTypeAndEnergy(structures,
+    STRUCTURE_SPAWN);
+  const towers = filterStructuresByTypeAndEnergy(structures,
+    STRUCTURE_TOWER);
+  const containers = filterContainersByStorage(structures);
+  return _.flatten([
+    extensions,
+    spawns,
+    towers,
+    containers
+  ]);
+}
+
+function filterStructuresByTypeAndEnergy(structures, structureType) {
+  return _.filter(structures, function(structure) {
+    return structure.structureType === structureType &&
+      structure.energy < structure.energyCapacity;
+  });
+}
+
+function filterContainersByStorage(structures) {
+  _.filter(structures, function(structure) {
+    return structure.structureType === STRUCTURE_CONTAINER &&
+      _.sum(structure.store) < 2000;
+  });
+}
 
 module.exports = roleHarvester;
