8b13673cafcac1cddb03d13e05a19157add8b9e0
jhegg <josh.hegg@gmail.com>
2016-12-03 23:50:46 -0800

    Decrease harvester size

--- creeps.templates.js
+++ creeps.templates.js
@@ -43,10 +43,10 @@ const upgraderBody = [
 ]; // cost: 750
 
 const extraLargeHarvesterBody = [
-  WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK,
+  WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK,
   CARRY,
   MOVE
-]; // cost: 1000
+]; // cost: 900
 const extraLargeBuilderBody = [
   WORK, WORK, WORK, WORK, WORK,
   CARRY, CARRY, CARRY, CARRY,
