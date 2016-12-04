2b1247959c7c7c6665fc3e87ac60be25a0a50149
jhegg <josh.hegg@gmail.com>
2016-12-04 14:09:06 -0800

    Fix: builders stuck if container empty

--- role.builder.js
+++ role.builder.js
@@ -43,8 +43,47 @@ var roleBuilder = {
 
       if (creep.memory.containerId !== undefined) {
         const container = Game.getObjectById(creep.memory.containerId);
-        if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(container);
+        const withdrawResult = creep.withdraw(container, RESOURCE_ENERGY);
+        switch (withdrawResult) {
+          case OK:
+            creep.memory.pickupWasEmptyCounter = undefined;
+            break;
+          case ERR_NOT_OWNER:
+            console.log(`Error: builder unable to transfer from ${container}
+              due to ownership/rampart`);
+            break;
+          case ERR_BUSY:
+            // still spawning
+            break;
+          case ERR_NOT_ENOUGH_RESOURCES:
+            if (!creep.memory.pickupWasEmptyCounter) {
+              creep.memory.pickupWasEmptyCounter = 1;
+            } else {
+              creep.memory.pickupWasEmptyCounter += 1;
+            }
+            if (creep.memory.pickupWasEmptyCounter > 4) {
+              creep.memory.containerId = undefined;
+              creep.memory.pickupWasEmptyCounter = undefined;
+            }
+            break;
+          case ERR_INVALID_TARGET:
+            console.log(`Error: builder ${creep} tried to harvest from
+              ${container} which is invalid`);
+            break;
+          case ERR_FULL:
+            console.log(`Warning: builder ${creep} was full but tried to harvest
+              anyway`);
+            break;
+          case ERR_NOT_IN_RANGE:
+            creep.moveTo(container);
+            break;
+          case ERR_INVALID_ARGS:
+            console.log(`Error: builder ${creep} tried to withdraw from
+               ${container} but resource amount or type was incorrect`);
+            break;
+          default:
+            console.log(`Warning: unknown result ${withdrawResult} from builder
+               withdraw`);
         }
         return;
       }
