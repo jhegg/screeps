1e0e043b1b48a9f973e0db5bbece58df4388e6d8
jhegg <josh.hegg@gmail.com>
2016-12-04 10:18:29 -0800

    Fixing emergency harvester spawning

--- creeps.spawner.js
+++ creeps.spawner.js
@@ -42,14 +42,14 @@ function spawnCreepFromTemplate(spawn, creepTemplates) {
       createCreep(spawn, template);
       break;
     } else if (template.role === 'harvester' &&
-      numCreeps.length < template.maxCreepsOfType) {
-      break;
-    } else if (template.role === 'harvester' &&
       numCreeps.length === 0 &&
       spawn.room.energyAvailable >= 200) {
         console.log('Emergency harvester spawn! Zero other harvesters present.');
         createCreep(spawn, { role: 'harvester', body: [WORK, CARRY, MOVE] });
         break;
+    } else if (template.role === 'harvester' &&
+      numCreeps.length < template.maxCreepsOfType) {
+      break;
     }
   }
 }
