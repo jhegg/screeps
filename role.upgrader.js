8ff1b1e626731664424215647365c3173e5ac295
jhegg <josh.hegg@gmail.com>
2016-12-04 12:08:22 -0800

    Fix upgraders getting stuck on empty containers

--- role.upgrader.js
+++ role.upgrader.js
@@ -21,8 +21,47 @@ var roleUpgrader = {
     } else {
       if (creep.memory.containerId !== undefined) {
         const container = Game.getObjectById(creep.memory.containerId);
-        if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(container);
+        const withdrawResult = creep.withdraw(container, RESOURCE_ENERGY);
+        switch (withdrawResult) {
+          case OK:
+            creep.memory.pickupWasEmptyCounter = undefined;
+            break;
+          case ERR_NOT_OWNER:
+            console.log(`Error: upgrader unable to transfer from ${container}
+              due to ownership/rampart`);
+            break;
+          case ERR_BUSY:
+            // still spawning
+            break;
+          case ERR_NOT_ENOUGH_RESOURCES:
+            if (!creep.memory.pickupWasEmptyCounter) {
+              creep.memory.pickupWasEmptyCounter = 1;
+            } else {
+              creep.memory.pickupWasEmptyCounter += 1;
+            }
+            if (creep.memory.pickupWasEmptyCounter > 4) {
+              creep.memory.containerId = undefined;
+              creep.memory.pickupWasEmptyCounter = undefined;
+            }
+            break;
+          case ERR_INVALID_TARGET:
+            console.log(`Error: upgrader ${creep} tried to harvest from
+              ${container} which is invalid`);
+            break;
+          case ERR_FULL:
+            console.log(`Warning: upgrader ${creep} was full but tried to harvest
+              anyway`);
+            break;
+          case ERR_NOT_IN_RANGE:
+            creep.moveTo(container);
+            break;
+          case ERR_INVALID_ARGS:
+            console.log(`Error: upgrader ${creep} tried to withdraw from
+               ${container} but resource amount or type was incorrect`);
+            break;
+          default:
+            console.log(`Warning: unknown result ${withdrawResult} from upgrader
+               withdraw`);
         }
         return;
       }
