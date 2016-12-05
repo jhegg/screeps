fc36e2a238e73fc908b0e39b8b05897101a6439f
jhegg <josh.hegg@gmail.com>
2016-12-04 19:35:07 -0800

    Add profiler

--- main.js
+++ main.js
@@ -7,45 +7,51 @@ var roomFinders = require('room.finders');
 var roomUtility = require('room.utility');
 var towerController = require('tower.controller');
 
+// https://github.com/gdborton/screeps-profiler
+const profiler = require('screeps-profiler');
+
+profiler.enable();
 module.exports.loop = function() {
-  creepsMaintainer.cleanOldCreepMemory();
+  profiler.wrap(function() {
+    creepsMaintainer.cleanOldCreepMemory();
 
-  for (var spawnName in Game.spawns) {
-    const room = Game.spawns[spawnName].room;
-    const roadsToRepair = roomFinders.findRoadsToRepair(room);
-    const hostileCreeps = roomFinders.findHostiles(room);
-    const constructionSites = roomFinders.findConstructionSites(room);
-    const droppedResources = roomFinders.findDroppedResources(room);
-    const energyStorageStructures = roomFinders.findEnergyStorageStructures(room);
-    const creepWorkData = {
-      constructionSites: constructionSites,
-      droppedResources: droppedResources,
-      energyStorageStructures: energyStorageStructures,
-    };
+    for (var spawnName in Game.spawns) {
+      const room = Game.spawns[spawnName].room;
+      const roadsToRepair = roomFinders.findRoadsToRepair(room);
+      const hostileCreeps = roomFinders.findHostiles(room);
+      const constructionSites = roomFinders.findConstructionSites(room);
+      const droppedResources = roomFinders.findDroppedResources(room);
+      const energyStorageStructures = roomFinders.findEnergyStorageStructures(room);
+      const creepWorkData = {
+        constructionSites: constructionSites,
+        droppedResources: droppedResources,
+        energyStorageStructures: energyStorageStructures,
+      };
 
-    creepsMaintainer.spawnNewCreeps(Game.spawns[spawnName]);
+      creepsMaintainer.spawnNewCreeps(Game.spawns[spawnName]);
 
-    for (var hostileCreep of hostileCreeps) {
-      // if any hostile creep is beyond 5 spaces of 50x50 room edges, activate
-      if (hostileCreep.pos.x > 5 ||
-        hostileCreep.pos.x < 45 ||
-        hostileCreep.pos.y > 5 ||
-        hostileCreep.pos.y < 45) {
-          roomUtility.activateSafeMode(room, hostileCreep);
+      for (var hostileCreep of hostileCreeps) {
+        // if any hostile creep is beyond 5 spaces of 50x50 room edges, activate
+        if (hostileCreep.pos.x > 5 ||
+          hostileCreep.pos.x < 45 ||
+          hostileCreep.pos.y > 5 ||
+          hostileCreep.pos.y < 45) {
+            roomUtility.activateSafeMode(room, hostileCreep);
+        }
+      }
+
+      towerController.run(room, hostileCreeps, roadsToRepair);
+
+      for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        assignSourceToCreep(creep);
+        if (shouldRetire(creep)) {
+          creepsMaintainer.retireOldCreep(creep);
+        }
+        putCreepToWork(creep, creepWorkData);
       }
     }
-
-    towerController.run(room, hostileCreeps, roadsToRepair);
-
-    for (var name in Game.creeps) {
-      var creep = Game.creeps[name];
-      assignSourceToCreep(creep);
-      if (shouldRetire(creep)) {
-        creepsMaintainer.retireOldCreep(creep);
-      }
-      putCreepToWork(creep, creepWorkData);
-    }
-  }
+  });
 };
 
 function assignSourceToCreep(creep) {
